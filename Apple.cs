84d47100596e70da28c91134d205b99d8b8b66a1
Susanna Makaryan <69461145+Susi77@users.noreply.github.com>
2020-09-05 16:43:23 +0400

    Add files via upload

---
+++ Apple.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializationIphone9
+{
+    public class Apple:IMobile
+    {
+        protected bool Chip = false;
+
+        public string Camera()
+        {
+            return ("They have one and more cameras .");
+        }
+
+        public string CPU()
+        {
+            return ("They have some kind of CPUs . ");
+        }
+
+        public string OperationSystem()
+        {
+            return ("They work with IOS. ");
+        }
+
+        public string RAM()
+
+        {
+            return ("Iphones have 2GB,4GB,6GB RAM . ");
+        }
+
+        public string SIM()
+        {
+            return ("Iphones have a SIM card or e-SIM .");
+        }
+
+        public string InternalMemory()
+        {
+
+            return ("They have 16GB,32GB,64GB,128GB,256GB internal memory . ");
+
+        }
+
+        
+    }
+}
---
+++ Iphone9.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializationIphone9
+{
+    public class Iphone9: Apple
+    {
+        public  new  string Camera()
+        {
+            return ("It has a camera .");
+        }
+
+        public  new string CPU()
+        {
+            return ("It has Apple A12Z Bionic CPU  . ");
+        }
+
+        public  new string OperationSystem()
+        {
+            return ("It warks with IOS. ");
+        }
+
+        public  new string RAM()
+
+        {
+            return ("It has 2GB RAM. ");
+        }
+
+        public  new string SIM()
+        {
+            return ("It has a SIM card.");
+        }
+
+        public new string InternalMemory()
+        {
+
+            return ("It has 32GB,64GB,128GB,256GB internal memory . ");
+
+        }
+
+        public bool UseProtectedMember()
+        {
+            return this.Chip;
+        
+        }
+     
+
+    }
+}
